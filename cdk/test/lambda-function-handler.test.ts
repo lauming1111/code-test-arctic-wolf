b28bfe8118642651068868c033c749a34451787f
imtmiml <ming_lau@cathaypacific.com>
2025-03-10 15:44:21 -0400

    test: add ans2 + ans 3 test cases

--- cdk/test/lambda-function-handler.test.ts
+++ cdk/test/lambda-function-handler.test.ts
@@ -1,12 +1,17 @@
 import { Context } from "aws-lambda/handler";
 import { handler } from "../lambda-functions/handler";
-import 'dotenv/config';
+import * as fs from "fs";
 
-;
+import 'dotenv/config';
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
 
 
 describe("Lambda Function Local Test", () => {
     let context: Context;
+    const expectedAnswer1 = JSON.parse(fs.readFileSync('./test/answer_1.json', 'utf8'));
+    const expectedAnswer2 = JSON.parse(fs.readFileSync('./test/answer_2.json', 'utf8'));
+    const expectedAnswer3 = JSON.parse(fs.readFileSync('./test/answer_3.json', 'utf8'));
     it("should get JSON from S3", async () => {
         // Test with no event data
         const response = await handler(null, context!);
@@ -16,5 +21,51 @@ describe("Lambda Function Local Test", () => {
         // console.log(JSON.stringify(response));
 
         // expect(JSON.parse(response.body)).toEqual({ message: "hello world" });
+    }, 3 * 60 * 1000);
+
+
+    it("should get answer 1 JSON from S3", async () => {
+        const params = {
+            Bucket: process.env.S3_BUCKET_NAME,
+            Key: 'answer_1.json',
+        };
+
+        // Fetch the JSON file from S3
+        const data = await s3.getObject(params).promise();
+        const jsonData = JSON.parse(data.Body.toString("utf-8"));
+        console.log(jsonData);
+
+        // Assert the response
+        expect(jsonData).toEqual(expectedAnswer1);
+    }, 3 * 60 * 1000);
+
+    it("should get answer 2 JSON from S3", async () => {
+        const params = {
+            Bucket: process.env.S3_BUCKET_NAME,
+            Key: 'answer_2.json',
+        };
+
+        // Fetch the JSON file from S3
+        const data = await s3.getObject(params).promise();
+        const jsonData = JSON.parse(data.Body.toString("utf-8"));
+        console.log(jsonData);
+
+        // Assert the response
+        expect(jsonData).toEqual(expectedAnswer2);
+    }, 3 * 60 * 1000);
+
+    it("should get answer 3 JSON from S3", async () => {
+        const params = {
+            Bucket: process.env.S3_BUCKET_NAME,
+            Key: 'answer_3.json',
+        };
+
+        // Fetch the JSON file from S3
+        const data = await s3.getObject(params).promise();
+        const jsonData = JSON.parse(data.Body.toString("utf-8"));
+        console.log(jsonData);
+
+        // Assert the response
+        expect(jsonData).toEqual(expectedAnswer3);
     }, 3 * 60 * 1000);
 });
